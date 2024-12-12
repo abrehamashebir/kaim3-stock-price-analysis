31ad0e4e78b886afae338e0e30b56a70ecce4531
abrehamashebir <abrisha96@gmail.com>
2024-12-12 18:32:08 +0300

    Perform sentiment analysis on headlines

---
+++ scripts/sentiment_score.py
@@ -0,0 +1,8 @@
+
+def categorize_sentiment(score):
+    if score > 0.05:
+        return 'Positive'
+    elif score < -0.05:
+        return 'Negative'
+    else:
+        return 'Neutral'
---
+++ scripts/word_analyer.py
@@ -0,0 +1,20 @@
+import nltk
+import re
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+
+def preprocess_text(text):
+    text = text.lower()
+    text = re.sub(r'[^\w\s]', '', text)
+    tokens = word_tokenize(text)
+    stop_words = set(stopwords.words('english'))
+    tokens = [w for w in tokens if not w in stop_words]
+    lemmatizer = WordNetLemmatizer()
+    tokens = [lemmatizer.lemmatize(w) for w in tokens]
+    return ' '.join(tokens)
