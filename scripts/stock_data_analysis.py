c9c5dfc32d6d9c567bc786050d9a2f585d7d9ec2
abrehamashebir <abrisha96@gmail.com>
2024-12-14 16:07:32 +0300

    Analysing and visualizing amazon stock data

--- scripts/stock_data_analysis.py
+++ scripts/stock_data_analysis.py
@@ -3,22 +3,7 @@ import pandas as pd
 import yfinance as yf
 import talib as ta
 
-class Finanicial_Data:
-    def __init__(self):
-        pass
-      
-    def __init__(self,ticker, start, end):
-        self.ticker =ticker
-        self.start=start
-        self.end =end
-    
-  
-    
-    def fetch_store_data(self):
-
-        return yf.download(ticker=self.ticker,start=self.start, end=self.end)
-    
-    def stock_moving_average(self,data, window):
+def stock_moving_average(data, window): 
         """
         Moving average of a given stock data price
 
@@ -31,18 +16,31 @@ class Finanicial_Data:
         """
 
         return ta.SMA(data, timeperiod =window)
-    
-    def data_loader(self,file_path):
 
+def RSI_indicator(data, window): 
         """
-         Load news data related to stock market
+        Moving average of a given stock data price
 
         Args:
-        file_path(str): Path to the CSV file
-    
-         Returns:
-           loaded data in the form of DataFrame  
-    
-          """
-        data = pd.read_csv(file_path)
-        return data
+           data(pd.series): the  data source
+           window: the window size 
+
+        Returns:
+              data rolling
+        """
+
+        return ta.RSI(data, timeperiod =window)
+
+def MACD_indicator(data): 
+        """
+        Moving average of a given stock data price
+
+        Args:
+           data(pd.series): the  data source
+           window: the window size 
+
+        Returns:
+              data rolling
+        """
+
+        return ta.MACD(data)
